be8c61568e1fa02f178b86df83677110a118fd43
Ron Gross <ron@navina.ai>
2021-04-07 21:41:15 +0300

    writing logs

--- index.js
+++ index.js
@@ -4,18 +4,22 @@ fs.config();
 const io = require("@actions/io");
 const puppeteer = require("puppeteer");
 
-const isDebug = false;
+const isDebug = true;
 let page;
 
 (async () => {
+  console.log('Launching puppeteer');
   const browser = await puppeteer.launch({
     headless: !isDebug,
     args: ["--no-sandbox"],
   });
+  console.log('launched');
   await io.mkdirP(`screenshots`);
+  console.log('new page');
   page = await browser.newPage();
   // page.on("console", (msg) => console.log("PAGE LOG:", msg.text()));
 
+  console.log('setting position');
   await page.evaluateOnNewDocument(function () {
     navigator.geolocation.getCurrentPosition = function (cb) {
       setTimeout(() => {
@@ -34,29 +38,44 @@ let page;
     };
   });
 
+  console.log('going to meckano');
   await page.goto("https://app.meckano.co.il/login.php");
+  console.log('waiting for selector email');
   await page.waitForSelector("#email", { visible: true, timeout: 5000 });
+  console.log('typing email');
   await page.type("#email", process.env.MECKANO_USER, { delay: 100 });
+  console.log('typing password');
   await page.type("#password", process.env.MECKANO_PASS, { delay: 100 });
+  console.log('clicking on connect');
   await page.click('[value="התחברות"]');
   await page.waitForNavigation({ timeout: 10000 });
 
   if (new Date().getHours() < 12) {
     //Checkin
-    if (!isDebug)
+    if (!isDebug) {
+      console.log('waiting rand time');
       await page.waitForTimeout(Math.floor(Math.random() * 15 * 60 * 1000));
+      console.log('finished waiting rand time');
+    }
+    console.log('waiting for wrapperCheckin');
     await page.waitForSelector(".wrapperCheckin");
+    console.log('clikcing on wrapperCheckin');
     await page.click(".wrapperCheckin");
 
     try {
+      console.log('1st question wait');
       await page.waitForSelector(".yesNo");
+      console.log('1st question select');
       await page.select(".yesNo", "כן");
+      console.log('1st question click');
       await page.click(".buttonNext");
 
       await page.waitForTimeout(3000);
-
+      console.log('2nd question wait');
       await page.waitForSelector(".yesNo");
+      console.log('2nd question select');
       await page.select(".yesNo", "כן");
+      console.log('2nd question click');
       await page.click(".buttonNext");
     } catch (e) {
       console.log(e);
@@ -70,6 +89,7 @@ let page;
   }
 
   await page.waitForTimeout(5000);
+  console.log('closing the browser');
   await browser.close();
 })()
   .catch((exception) => {
